6eb78cf2e741ad981b6c00d12d71598ab60b4edb
mis7ake7411 <feng.school@gmail.com>
2024-08-19 22:17:42 +0800

    init: 初始化

---
+++ src/main/java/com/mis7ake7411/tddprojectdemo/TddProjectDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mis7ake7411.tddprojectdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddProjectDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddProjectDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mis7ake7411/tddprojectdemo/TddProjectDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mis7ake7411.tddprojectdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddProjectDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
